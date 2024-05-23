7400f6458fac86b7e457bb828cdc092f6b736a9d
mbinali <mbinali@visionbank.com.sa>
2024-05-23 12:37:35 +0300

    Order service - added logs and exception and properties

---
+++ src/main/java/com/mezocode/order/exception/ProductAvailabilityException.java
@@ -0,0 +1,8 @@
+package com.mezocode.order.exception;
+
+public class ProductAvailabilityException extends RuntimeException {
+
+    public ProductAvailabilityException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/mezocode/order/service/OrderService.java
+++ src/main/java/com/mezocode/order/service/OrderService.java
@@ -1,6 +1,7 @@
 package com.mezocode.order.service;
 
 import com.mezocode.order.client.InventoryClient;
+import com.mezocode.order.exception.ProductAvailabilityException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +17,14 @@ public class OrderService {
     public void processOrder(Long productId, long quantity) {
         System.out.println("processOrder -- calling the inventoryService with productId: "+productId + ", and quantity: "+quantity);
         Boolean isProductAvailable = inventoryClient.checkProductAvailability(productId);
+        if (!isProductAvailable) {
+            throw new ProductAvailabilityException("Product:"+ productId+" unavailable");
+        }
 
     }
 
     public void fallbackCheckInventory(Long productId, long quantity, Throwable throwable) {
         System.out.println("Fallback logic if Inventory Service fails for productId "
-                + productId + ". Error: " + throwable.getMessage());
+                + productId + " and quantity : "+quantity+ ". Error: " + throwable.getMessage());
     }
 }
